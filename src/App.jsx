4c1c7b95da2b3569f8a01b76d7d6030550f56e93
ixtk <ikhcamot@gmail.com>
2025-01-10 22:11:50 +0400

    Add cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import desserts from "./data.json";
 import { Dessert } from "./components/Dessert";
+import { RemoveItem } from "./Icons";
 
 function App() {
   return (
@@ -13,6 +14,56 @@ function App() {
               return <Dessert {...dessertObj} key={dessertObj.name} />;
             })}
           </div>
+        </div>
+        <div className="cart">
+          <h2>Your cart (7)</h2>
+          <ul>
+            <li>
+              <p className="title">Classic Teiramisu</p>
+              <span className="quantity">1x</span>
+              <span className="price">@$5.50</span>
+              <span className="total">$5.50</span>
+              <button className="remove-btn">
+                <RemoveItem />
+              </button>
+            </li>
+            <li>
+              <p className="title">Classic Teiramisu</p>
+              <span className="quantity">1x</span>
+              <span className="price">@$5.50</span>
+              <span className="total">$5.50</span>
+              <button className="remove-btn">
+                <RemoveItem />
+              </button>
+            </li>
+            <li>
+              <p className="title">Classic Teiramisu</p>
+              <span className="quantity">1x</span>
+              <span className="price">@$5.50</span>
+              <span className="total">$5.50</span>
+              <button className="remove-btn">
+                <RemoveItem />
+              </button>
+            </li>
+            <li>
+              <p className="title">Classic Teiramisu</p>
+              <span className="quantity">1x</span>
+              <span className="price">@$5.50</span>
+              <span className="total">$5.50</span>
+              <button className="remove-btn">
+                <RemoveItem />
+              </button>
+            </li>
+            <li>
+              <p className="title">Classic Teiramisu</p>
+              <span className="quantity">1x</span>
+              <span className="price">@$5.50</span>
+              <span className="total">$5.50</span>
+              <button className="remove-btn">
+                <RemoveItem />
+              </button>
+            </li>
+          </ul>
         </div>
       </main>
     </div>
