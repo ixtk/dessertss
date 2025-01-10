42241721a9042d163550298e6caccc1fcd9486bd
ixtk <ikhcamot@gmail.com>
2025-01-10 21:50:04 +0400

    Style dessert container

--- src/components/Dessert.jsx
+++ src/components/Dessert.jsx
@@ -1,15 +1,29 @@
 import { AddToCart } from "../Icons";
 
-export const Dessert = () => {
+const BASE_URL = "https://res.cloudinary.com/dc2c49xov/desserts";
+
+export const Dessert = ({ images, name, category, price }) => {
   return (
     <div className="dessert">
-      <img
-        src="https://res.cloudinary.com/dc2c49xov/desserts/creme-brulee-mobile.jpg"
-        alt=""
-      />
+      <picture>
+        <source
+          media="(min-width:650px)"
+          srcset={`${BASE_URL}/${images.tablet}`}
+        />
+        <source
+          media="(min-width:465px)"
+          srcset={`${BASE_URL}/${images.mobile}`}
+        />
+        <img src={`${BASE_URL}/${images.desktop}`} />
+      </picture>
       <button>
         <AddToCart /> Add to cart
       </button>
+      <div className="details">
+        <span className="category">Waffle</span>
+        <p className="title">Waffle with Berries</p>
+        <p className="price">$6.50</p>
+      </div>
     </div>
   );
 };
